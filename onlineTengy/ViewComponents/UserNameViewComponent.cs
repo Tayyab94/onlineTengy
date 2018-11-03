9a04cac51d8f3e7de587197da98eb1faf7ec0b17
Hp <Hp@DESKTOP-HSQQ1VE>
2018-11-03 01:37:12 -0700

    AddViewComponentModelAndConsuming

---
+++ onlineTengy/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using onlineTengy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace onlineTengy.ViewComponents
+{
+    public class UserNameViewComponent:ViewComponent
+    {
+        private ApplicationDbContext _context;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claim = (ClaimsIdentity)this.User.Identity;
+            var cl = claim.FindFirst(ClaimTypes.NameIdentifier);
+
+            var UserDb = await _context.Users.Where(x => x.Id == cl.ValueType.ToString()).FirstOrDefaultAsync();
+
+            return View(UserDb);
+        }
+    }
+}
